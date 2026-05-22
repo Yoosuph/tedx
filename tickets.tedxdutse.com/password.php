62dc46ebe9de5e3fe31fa9a31974c0896cb65ad9
Yoosuph <ylawan658@gmail.com>
2026-05-22 18:23:02 +0100

    fixed

--- tickets.tedxdutse.com/password.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = 'theTEDx'; // Change this to your desired password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-echo "Hashed password: " . $hashed_password;
-?>
